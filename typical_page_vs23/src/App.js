4f4ecd021a5fc9b1d19d6b8be672ff4ef9fb2e8c
karlteine <karlkivisild@gmail.com>
2024-03-22 14:23:22 +0200

    User login and logout with localStorage

---
+++ typical_page_vs23/src/App.js
@@ -0,0 +1,52 @@
+import { Fragment, useState, useEffect } from 'react';
+import MainHeader from './components/MainHeader/MainHeader';
+import Login from './components/Login/Login';
+import Home from "./components/Home/Home";
+
+function App() {
+  const [loggedIn, setLoggedIn] = useState(() => {
+    if(JSON.parse(localStorage.getItem('isLoggedUser')) !== null) {
+      return JSON.parse(localStorage.getItem('isLoggedUser')).isLogged
+    } else {
+      return false;
+    }
+  })
+  console.log(loggedIn)
+
+  useEffect(() => {
+    const storedLoggedUserData = JSON.parse(localStorage.getItem
+    ('isLoggedUser'))
+    if(storedLoggedUserData !== null){
+      if(storedLoggedUserData.isLogged === true){
+        setLoggedIn(true)
+      }
+    }
+  }, [])
+
+  const loginHandler = (user, password) => {
+    const loggedUser = localStorage.setItem('isLoggedUser', JSON.
+    stringify({
+      username: user,
+      isLogged: true
+    }))
+    setLoggedIn(true)
+  }
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedUser')
+    setLoggedIn(false)
+  } 
+
+  
+  return (
+    <Fragment>
+      <MainHeader isAuthenticated={loggedIn} onLogout={logoutHandler} />
+      <main>
+        {!loggedIn && <Login onLogin={loginHandler}/>}
+        {loggedIn && <Home/>}  
+      </main>
+    </Fragment>
+  );
+} 
+
+export default App;
---
+++ typical_page_vs23/src/components/MainHeader/Navigation.js
@@ -0,0 +1,26 @@
+import './Navigation.css'
+import Button from '../UI/Button'
+import React from 'react';
+
+
+const Navigation = (props) => {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <a href="/users">Users</a>
+                </li>
+                <li>
+                    <a href="/admin">Admin</a>
+                </li>
+               {props.loggedIn && (
+                    <li>
+                        <Button onClick={props.onLogout}>Logout</Button>
+                    </li>
+               )}  
+            </ul>
+        </nav>
+    )
+}
+
+export default Navigation
